6ccf518e39c76a5be432112add5cbbee066d72e3
PBoyal20 <129793660+PBoyal20@users.noreply.github.com>
2023-12-06 15:55:56 -0800

    Update input.js

--- input.js
+++ input.js
@@ -2,6 +2,9 @@ export class InputHandler{
     constructor(game){
         this.game = game;
         this.keys = [];
+        this.touchY = '';
+        this.touchX = '';
+        this.touchThreshold = 30;
         window.addEventListener('keydown', e => {
             if((e.key === 'ArrowDown' ||
                e.key === 'ArrowUp' ||
@@ -27,5 +30,38 @@ export class InputHandler{
              }
             
         });
+
+         window.addEventListener('touchstart', e => {
+               this.touchY = e.changedTouches[0].pageY;
+               this.touchX = e.changedTouches[0].pageX;
+         });
+
+         window.addEventListener('touchmove', e => {
+            const swipeDistanceY = e.changedTouches[0].pageY - this.touchY;
+            const swipeDistanceX = e.changedTouches[0].pageX - this.touchX;
+
+            if(swipeDistanceY < -this.touchThreshold && this.keys.indexOf('SwipeUp') === -1){
+                this.keys.push('SwipeUp');
+            }
+            else if(swipeDistanceY > this.touchThreshold && this.keys.indexOf('SwipeDown') === -1){
+                this.keys.push('SwipeDown');
+
+                //add possible swipe down to restart game here   
+            }  
+
+            if (swipeDistanceX < -this.touchThreshold && this.keys.indexOf('SwipeLeft') === -1){
+               this.keys.push('SwipeLeft');
+            }
+            else if(swipeDistanceX > this.touchThreshold && this.keys.indexOf('SwipeRight') === -1){
+               this.keys.push('SwipeRight');
+            }
+         });
+
+         window.addEventListener('touchend', e => {
+            this.keys.splice(this.keys.indexOf('SwipeUp'),1);
+            this.keys.splice(this.keys.indexOf('SwipeDown'),1);
+            this.keys.splice(this.keys.indexOf('SwipeLeft'),1);
+            this.keys.splice(this.keys.indexOf('SwipeRight'),1);
+         });
     }
 }
